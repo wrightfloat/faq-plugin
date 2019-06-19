bdf778c8aeca87f0bd7f45cc3990739f3a12b022
wrightfloat <wrightfloat@hotmail.com>
2019-06-19 11:20:29 -0500

    Added a taxonomy template

---
+++ templates/taxonomy-dlwfq_topics.php
@@ -0,0 +1,104 @@
+    <?php 
+        /**
+        * The Template for displaying our faq topics taxonomy.
+        * @package Faqizer
+        * @since   0.2.0 
+        */
+        if ( ! defined( 'ABSPATH' ) ) {
+            exit;
+        }
+    ?>
+    
+    <?php get_header(); ?>
+    <div id="faqs-container" class="faq-plugin-template">
+
+    <section>
+        <header class="faq-header">
+            <h1 class="faq-header-title page-title">
+                <?php echo esc_html( dlwfq_echo_archive_title('taxonomy') ); ?>
+            </h1>
+        </header>
+    </section>
+
+    <?php
+
+        $args = array(
+            'post_type'       => 'dlw_wp_faq',
+            'post_status'     => 'publish',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'dlwfq_topics',
+                    'field'    => 'slug',
+                    'terms'    => get_query_var( 'faq-topics' ), //grabs the current term for our custom taxonomy.
+                ),
+            ),
+            'paged'           => get_query_var( 'paged' ),
+            'posts_per_page'  => esc_attr( dlwfq_get_the_archive_post_count() ), //this has to sync with the default posts per page 
+        );
+        // the query
+        $the_query = new WP_Query( $args ); 
+    ?>
+
+    <?php if ( $the_query->have_posts() ) : ?>
+
+        <ul id="basics" class="dlwfq-fq-list">
+        <!-- the loop -->
+        <?php
+            $counter = -1; 
+            while ( $the_query->have_posts() ) : $the_query->the_post();  $counter++; 
+        ?> 
+
+        <li class="dlwfq-fq-target" data-content-status="closed" data-index="<?php echo $counter; ?>">
+            <span class="dlwfq-fq-wrap">
+                
+                <?php 
+                    //if the user does not have accordian enabled a link will be clickable, which will take the user to the single faq page. 
+                    if( dlwfq_get_accordian_settings() !== true){ 
+                ?>
+                <a class="dlwfq-fq-target" href="<?php the_permalink(); ?>">
+                    <?php the_title(); ?>
+                </a>
+
+                <?php }
+                    //When the user has the accordian enabled no link will appear in the title area of the faq.
+                else{
+                    the_title();
+                ?>
+
+                <span class="dlwfq-fq-icons">
+                    <i class="fa fa-angle-down" aria-hidden="true" style="display: block; line-height: 48px;position: absolute;top: 0px;right: 1rem;"></i>
+                </span>
+
+                <?php } ?>
+            </span>
+
+            <div class="dlwfq-fq-content">
+                <p><?php esc_html( the_content() ) ;?></p>
+            </div> <!-- dlwfq-fq-content-->
+        </li> 
+        <?php endwhile; ?>
+        <!-- end of the loop -->
+        </ul>
+        <div id="dlwfq-pagination">
+            <!-- pagination here -->
+            <?php echo paginate_links( array( 'total' => $the_query->max_num_pages) ); ?>
+        </div>
+        <?php wp_reset_postdata(); ?>
+        
+    <?php else: 
+        
+        ?>
+        <ul id="basics" class="dlwfq-fq-list">
+            <li class="dlwfq-fq-target" data-content-status="closed" data-index="1">
+                <span class="dlwfq-fq-target">
+                <?php 
+                    //Make this editable via backend settings, Also needs documentation added too support form. 
+                    esc_html_e( __('Sorry, No FAQS To Display here', 'dlwfq_faqizer') );
+                ?>
+                </span>
+            </li>
+        </ul>
+    <?php endif; ?>
+    </div>
+
+    <?php get_footer(); ?>
